d07cdf65cc50a7ed8693aaf2d83efbc81e3b917b
Manuele <manuele.tamburrano@gmail.com>
2026-08-30 21:28:09 +0200

    Publish the verified Proof Board delivery artifact

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { createTodo, getOpenTodos } from "../dist/index.js";
+import { completeTodo, createTodo, getOpenTodos } from "../dist/index.js";
 
 test("createTodo creates an open todo", () => {
   assert.deepEqual(createTodo(1, "Ship the demo"), {
@@ -22,4 +22,16 @@ test("getOpenTodos returns only incomplete todos", () => {
     getOpenTodos(todos).map((todo) => todo.id),
     [1, 3],
   );
+});
+
+test("completeTodo marks a todo as completed", () => {
+  const todo = createTodo(1, "Ship the demo");
+
+  assert.deepEqual(completeTodo(todo), {
+    id: 1,
+    title: "Ship the demo",
+    completed: true,
+  });
+  assert.equal(todo.completed, false);
+  assert.deepEqual(getOpenTodos([completeTodo(todo)]), []);
 });
